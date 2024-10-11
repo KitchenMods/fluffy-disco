8336197d09f60a3c5ab81a8832a90db0c33ba35c
Lachlan Leone <starfluxgames@gmail.com>
2024-10-11 14:09:24 +1100

    test

---
+++ KitchenLib.TestMod/Main.cs
@@ -0,0 +1,9 @@
+namespace KitchenLib.TestMod
+{
+    public class Main {
+    }
+	/*
+	* This is a custom AudioClip returned for the custom process 'rollProcess' - This needs to be added to KitchenLib
+	*/
+	
+}
---
+++ KitchenLib.TestMod/TestingTerminalAppliance.cs
@@ -0,0 +1,3 @@
+namespace KitchenLib.TestMod
+{
+}
