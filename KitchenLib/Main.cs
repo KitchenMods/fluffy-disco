1c3ceb9f1608762b200cbf7f4e7d9d1abb24f987
Lachlan Leone <starfluxgames@gmail.com>
2024-10-11 16:51:17 +1100

    ccc

--- KitchenLib/Main.cs
+++ KitchenLib/Main.cs
@@ -49,7 +49,12 @@ namespace KitchenLib
 		/// <summary>
 		/// The beta version of the mod.
 		/// </summary>
-		internal const string MOD_BETA_VERSION = "2";
+		internal const string THE_MOD_BETA_VERSION = "2";
+#if DEBUG
+		internal static string MOD_BETA_VERSION = THE_MOD_BETA_VERSION + "-DEBUG";
+#else
+		internal static string MOD_BETA_VERSION = THE_MOD_BETA_VERSION;
+#endif
 
 		/// <summary>
 		/// The compatible versions of the mod.
@@ -101,7 +106,7 @@ namespace KitchenLib
 		/// </summary>
 		/// <param name="mod">The mod instance.</param>
 		protected override void OnPostActivate(Mod mod)
-		{	
+		{
 			Logger = InitLogger();
 			
 			manager = new PreferenceManager(MOD_ID);
