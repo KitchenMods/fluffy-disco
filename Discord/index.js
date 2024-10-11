a788998c29cfeb89d89567052deafe45f7258aaf
Lachlan Leone <starfluxgames@gmail.com>
2024-10-11 17:59:52 +1100

    x

--- Discord/index.js
+++
@@ -1,6 +0,0 @@
-const { Webhook } = require('discord-webhook-node');
-const hook = new Webhook("https://discord.com/api/webhooks/1270786942350524509/Pt6B41jSv9EewQqPbJMOYQR9-UHvQvYeXDG8uwAYGITaIbpTt4PXWBHNLpwiNaimk83G");
- 
-hook.setUsername('Discord Webhook Node Name');
- 
-hook.send("Hello there!");
